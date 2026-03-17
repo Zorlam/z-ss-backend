bdb9d7ac410d0e3311ed9dbfa6a54037d90cf59b
乙 <164361038+Zorlam@users.noreply.github.com>
2026-03-17 21:52:33 +0100

    Remove Flask-Migrate import from extensions.py
    Removed unused import for Flask-Migrate.

--- app/extensions.py
+++ app/extensions.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
@@ -10,7 +9,6 @@ db = SQLAlchemy()
 jwt = JWTManager()
 bcrypt = Bcrypt()
 cors = CORS()
-migrate = Migrate()
 limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["200 per day", "50 per hour"],
